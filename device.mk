bd81700e7d57a6127c496d3e2b0a26e290d6c0e7
Edwin Wong <edwinwong@google.com>
2018-02-22 22:18:29 -0800

    land: Put move_widevine_data.sh in /system/bin.
    Add dependency to install move_widevine_data.sh to /system/bin.
    
    The script is only needed for existing devices that
    are running Widevine DRM and are upgrading to Pi or later
    Android releases.
    
    Test: Upgrade from O-MR1 to Pi, Netflix & Play Movies
      can play back offline content downloaded in O-MR1
    
    Test: adb shell to verify script is in /system/bin
    
    Test: adb shell to verify widevine data is moved to
      /data/vendor/mediadrm
    
    Test: use audit2allow to verify no avc:denied for widevine drm service
    
    bug: 73656300
    Change-Id: I487d6d0b7048a7b663ce1c29fa5d3ea3cd4f3fbb

--- device.mk
+++ device.mk
@@ -183,7 +183,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-service.widevine \
-    libprotobuf-cpp-lite
+    libprotobuf-cpp-lite \
+    move_widevine_data.sh
 
 # Fingerprint
 PRODUCT_PACKAGES += \
